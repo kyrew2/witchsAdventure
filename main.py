97828e102d2a5d80b17f2cfdacbad51cdf753356
Pedro Henrique Franceschi <pedrohenriquekyre@gmail.com>
2025-06-06 17:52:12 -0300

    consegui fazer o pulo

--- main.py
+++ main.py
@@ -28,13 +28,16 @@ posicaoBruxaY = 500
 movimentoBruxaX = 0
 movimentoBruxaY = 0
 gravidade = 1
+chaoY = 600
+chaoFase = pygame.Rect(0, chaoY, fase, 100)
+
+
 pula = False
 
 
 #Loop Principal do Jogo
 rodando = True
 while rodando:
-    #Verifica os eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
@@ -48,22 +51,21 @@ while rodando:
             movimentoBruxaX = 0
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP and not pula:
             pula = True
-            movimentoBruxaY -=20
-        elif evento.type == pygame.KEYUP and evento.key == pygame.K_UP:
-            pula = False
-            movimentoBruxaY += 20
+            movimentoBruxaY = -20
 
-
+    movimentoBruxaY += gravidade
     posicaoBruxaX +=movimentoBruxaX
     posicaoBruxaY +=movimentoBruxaY
-    
-    pygame.draw.rect(tela, branco, (5000, 550, 1000, 150)) #desenha o chão
-    if pula:
-        movimentoBruxaY += gravidade
-    else:
+    colisaoBruxa = pygame.Rect(posicaoBruxaX, posicaoBruxaY, bruxa.get_width(), bruxa.get_height())
+    if colisaoBruxa.colliderect(chaoFase):
+        posicaoBruxaY = chaoY - bruxa.get_height()
         movimentoBruxaY = 0
-    tela.fill(preto)
-    tela.blit(bruxa, (posicaoBruxaX, posicaoBruxaY))
+        pula = False
 
+
+    tela.fill(preto)
+    pygame.draw.rect(tela, branco, chaoFase)
+    tela.blit(bruxa, (posicaoBruxaX, posicaoBruxaY))
+    
     pygame.display.flip()
     relogio.tick(60) #Limita a 60 FPS
