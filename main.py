b5bf9843debf374e5ba572cd15104c495a45dbac
Pedro Henrique <pedrohenriquekyre@gmail.com>
2025-06-06 10:20:23 -0300

    primeira versão mas é só um teste

---
+++ main.py
@@ -0,0 +1,65 @@
+# Importa as bibliotecas necessárias
+import pygame
+import os
+import recursos
+
+
+#inicializa o pygame
+pygame.init()
+ 
+#Cria a janela do jogo
+tela = pygame.display.set_mode((1000, 700))
+pygame.display.set_caption("Jogo de plataforma")
+
+fase = 5000
+
+#Cores
+preto = (0, 0, 0)
+branco = (255,  255, 255)
+
+relogio = pygame.time.Clock()
+
+
+
+#jogador
+bruxa = pygame.image.load("recursos/bruxa.png")
+posicaoBruxaX = 100
+posicaoBruxaY = 500
+movimentoBruxaX = 0
+movimentoBruxaY = 0
+pula = False
+
+
+
+#Loop Principal do Jogo
+rodando = True
+while rodando:
+    #Verifica os eventos
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_RIGHT:
+            movimentoBruxaX = 15
+        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_LEFT:
+            movimentoBruxaX = -15
+        elif evento.type == pygame.KEYUP and (evento.key == pygame.K_RIGHT or evento.key == pygame.K_LEFT):
+            movimentoBruxaX = 0  
+        elif evento.type == pygame.KEYUP and evento.key == pygame.K_LEFT:     
+            movimentoBruxaX = 0
+        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP and not pula:
+            pula = True
+            movimentoBruxaY -=20
+        elif evento.type == pygame.KEYUP and evento.key == pygame.K_UP:
+            pula = False
+            movimentoBruxaY += 30
+
+
+    posicaoBruxaX +=movimentoBruxaX
+    posicaoBruxaY +=movimentoBruxaY
+    
+
+    tela.fill(preto)
+    tela.blit(bruxa, (posicaoBruxaX, posicaoBruxaY))
+
+    pygame.display.flip()
+    relogio.tick(60) #Limita a 60 FPS
